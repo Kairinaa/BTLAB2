f05e72e17813832996128baa7caa773ebea769d9
Kairinaa <dkhai0309@gmail.com>
2023-03-04 23:46:04 +0700

    LAB24

---
+++ LAB24/Cylinder.java
@@ -0,0 +1,24 @@
+package LAB24;
+
+public class Cylinder extends Circle {
+    public Cylinder(double radius, String color) {
+        super(radius, color);
+    }
+    private double height;
+
+    public Cylinder(double radius, String color, double height) {
+        super(radius, color);
+        this.height = height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+    public double getVolume() {
+        return Area() * height;
+    }
+}
